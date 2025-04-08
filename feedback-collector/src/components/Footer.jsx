3695cf40da3a3dfe5ec78f6a2547d277af5c4ecb
prathameshshetty <prathamesh.cse@gmail.com>
2025-04-08 22:31:07 +0530

    Created Footer

---
+++ feedback-collector/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+// src/components/Footer.jsx
+const Footer = () => {
+    return (
+      <footer className="text-center py-4 text-sm text-gray-500 dark:text-gray-400">
+        © {new Date().getFullYear()} Feedback Collector App. Crafted with ❤️ by Prathamesh Shetty.
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
